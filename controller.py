f7f9b6815942f7252554930ec6870562062ea11b
LarisaZaripova <larik7lolik@gmail.com>
2022-05-21 08:43:45 +0300

    initial commit

---
+++ controller.py
@@ -0,0 +1,15 @@
+import calc_complex
+import calc_float
+import ui
+import log
+
+def run():
+    temp = ui.initial()
+    if 'j' in temp[0] and 'j' in temp[1]:
+          result=calc_complex.calc(temp[0], temp[1],temp[2])
+    else:
+           result=calc_float.calc(temp[0], temp[1],temp[2])
+    print(result)
+    log.log_to_file(temp, result)
+    return result 
+    
